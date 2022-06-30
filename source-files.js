e68ce6994439f5e80f2b0ec0ea76264809bd0a44
alexschrod <alexschrod@gmail.com>
2022-06-30 20:22:03 +0000

    Deploying to gh-pages from @ alexschrod/encoding-next@cc24035f53ce6361736ea1f0705b9fb9d527aef3 🚀

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var sourcesIndex = {};
+sourcesIndex["encoding"] = {"name":"","dirs":[{"name":"codec","files":["ascii.rs","error.rs","japanese.rs","korean.rs","simpchinese.rs","singlebyte.rs","tradchinese.rs","utf_16.rs","utf_8.rs","whatwg.rs"]}],"files":["all.rs","label.rs","lib.rs","types.rs","util.rs"]};
+createSourceSidebar();
